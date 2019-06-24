8aa6cdb34d590c6f239dce4e70eb74b01fb43a85
simonxxf <995546883@qq.com>
2019-06-24 09:12:09 +0800

    work

--- vue-select/src/App.vue
+++ vue-select/src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <my-select :zipInfo="zipInfo"></my-select>
+   <my-select :zipInfo="zipInfo"></my-select>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import mySelect from "./components/my-select"
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   
+  },
+  data () {
+    return {
+      zipInfo: [
+        {
+            name: '基隆市',
+            areas: [
+                { name: '仁愛區', zip: '200' },
+                { name: '信義區', zip: '201' },
+                { name: '中正區', zip: '202' },
+                { name: '中山區', zip: '203' },
+                { name: '安樂區', zip: '204' },
+                { name: '暖暖區', zip: '205' },
+                { name: '七堵區', zip: '206' },
+            ],
+        },
+        {
+            name: '台北市',
+            areas: [
+                { name: '中正區', zip: '300' },
+                { name: '大同區', zip: '301' },
+                { name: '中山區', zip: '302' },
+                { name: '松山區', zip: '303' },
+                { name: '大安區', zip: '304' },
+                { name: '萬華區', zip: '305' },
+                { name: '信義區', zip: '306' },
+                { name: '士林區', zip: '307' },
+                { name: '北投區', zip: '308' },
+                { name: '內湖區', zip: '309' },
+                { name: '南港區', zip: '310' },
+                { name: '文山區', zip: '311' },
+            ],
+        },
+        {
+            name: '新竹市',
+            areas: [
+                { name: '新竹市', zip: '400' },
+            ],
+        },
+      ],
+    }
+  },
+  computed:{
+    
+  },
+  components:{
+    mySelect
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+display: flex 
 }
+
 </style>
---
+++ vue-select/src/components/my-select.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="city">
+    <select >
+      <option v-for="item in zipInfo" >
+        {{item.name}}
+      </option>
+    </select>
+       
+    
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+   props: {
+     zipInfo:{
+       type:  Array
+     }
+   },
+  data(){
+    return {
+    }
+  },
+}
+</script>
+<style lang='stylus' scoped>
+</style>
